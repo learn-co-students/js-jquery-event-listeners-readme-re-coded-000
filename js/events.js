54ee636b6556d96aa0287ee89be98775d1b743dc
NOOR <noor.s.suliman@gmail.com>
2017-02-23 21:34:39 +0300

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,49 @@
 //define functions here
 
+function getIt() {
+
+	     $('p').on("click",function(){
+
+	    alert('Hey!');		
+	});
+}
+
+function frameIt(){
+
+	$('img').on("load",function(){
+
+		$('img').addClass("tasty");
+	});
+}
+
+function pressIt(){
+
+	$('input').on("keydown",function(key){
+
+		  if (key.which == 71){
+
+			alert("you are pressing g");
+		};
+	});
+}
+
+function submitIt(){
+	$("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
+}
 $(document).ready(function(){
 
 // call functions here
 
+   getIt();
+   frameIt();
+   pressIt();
+   submitIt();
 });
+
