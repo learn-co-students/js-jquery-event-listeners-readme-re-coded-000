796c14aca41a3a0ed8acf05be92f11cdefe93f28
Ekhlas Rain <baran.rain1994@gmail.com>
2017-03-04 15:10:55 +0300

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,31 @@
 //define functions here
+function getIt(){
+	$('p').click("Hey!");
+
+}
+
+function frameIt(){
+	$('img').on('load', function(){
+	  $('img').addClass('tasty')
+
+	});
+}
+
+function pressIt(){
+	$('#typing').on('keydown', function(key) {
+    // 'g' corresponds to 83
+    if (key.which === 83) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(key) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 
 $(document).ready(function(){
 
