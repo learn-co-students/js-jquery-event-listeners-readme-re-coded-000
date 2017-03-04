8a6af7271c76a7bf610ed190e2f20d427900ec5a
Saber Rasheed <edrees.rasheed@yahoo.com>
2017-03-04 15:01:05 +0300

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
-
+function getIt() {
+  $('p').on('click',function functionName() {
+     alert("Hey!")
+  })
+}
+function frameIt() {
+  $('img').on('load',function functionName() {
+     $('img').addClass('tasty')
+  })
+}
+function pressIt() {
+  $('input').on('keydown',function functionName(event) {
+     if(event.which==71){alert("oops")}
+  })
+}
+function submitIt() {
+  $('form').on('submit',function functionName() {
+     alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
-
+ getIt();
+ frameIt();
+ pressIt();
+ submitIt()
 // call functions here
 
 });
