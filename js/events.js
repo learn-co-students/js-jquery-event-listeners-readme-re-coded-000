c1a5b9a7139b29f013ead952a3dac3cb9804f15f
Muataz Aziz <muataz.aziz91@yahoo.com>
2017-03-10 17:23:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,33 @@
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
 
 });
+
+
+function getIt(){
+  $('p').on('click', function(){
+    alert ("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown' , function(key){
+    if (key.which === 71){
+      alert('g has been pressed')
+    }
+  });
+}
+
+function submitIt(){
+$("form").on("submit", function() {
+    alert('your form is going to be submitted now');
+  });
+}
