62844277035fc5aaed5f496651c53019107e3326
Mohammad Daylaki <github email address>
2017-03-11 10:45:59 +0300

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+  $("p").click(function(){
+    alert("Hey!");
+  })
+}
 
+function frameIt() {
+  $('img').load(function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').keydown(function(key){
+    if (key.which == 71) {
+      alert('g is pressed')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').submit(function(){
+    alert("your form is going to be submitted now");
+    return;
+  })
+}
 $(document).ready(function(){
-
+getIt();
 // call functions here
-
+frameIt();
+pressIt();
+submitIt();
 });
